8abcff085ba4b00252b4a9ddf8356813fe74b875
reneyami <yamihellyeah@gmail.com>
2022-12-24 21:59:06 -0600

    Se añade la parte de función

--- mini_calculadora.py
+++ mini_calculadora.py
@@ -1,24 +1,25 @@
-print("Programa\n")
-try:
-    a1 = int(input("introduce un primer número: "))
-    a2 = int(input("introduce un segundo número: "))
+print("Programa")
+def calculadora(eleccion):
+    try:
+        a1 = int(input("introduce un primer número: "))
+        a2 = int(input("introduce un segundo número: "))
 
-    eleccion = int(input("que quieres hacer?\n1.-suma\n2.-resta\n3.-division\n"))
+        eleccion = int(input("que quieres hacer?\n1.-suma\n2.-resta\n3.-division\n"))
 
-    if eleccion == 1:
-        a3 = a1 + a2
-        print(f"la suma es {a3}")
-    elif eleccion == 2:
-        a3 = a1 - a2
-        print(f"la resta es {a3}")
-    elif eleccion == 3:
-        a3 = a1 / a2
-        print(f"la division es: {a3}")
-    else:
-        print(f"no existe")
-except ZeroDivisionError:
-        result = 0
-        print(f"el resultado es: {result}")
-except:
-    print(f"no debes usar letras, sino números")
-        
+        if eleccion == 1:
+            a3 = a1 + a2
+            print(f"la suma es {a3}")
+        elif eleccion == 2:
+            a3 = a1 - a2
+            print(f"la resta es {a3}")
+        elif eleccion == 3:
+            a3 = a1 / a2
+            print(f"la division es: {a3}")
+        else:
+            print(f"no existe")
+    except ZeroDivisionError:
+            result = 0
+            print(f"el resultado es: {result}")
+    except:
+        print(f"no debes usar letras, sino números")
+
